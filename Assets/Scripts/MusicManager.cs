315536afa222b4179e75c4f550f6355c74e92b5c
spectralrook <spectralrook@gmail.com>
2016-11-17 16:13:37 -0500

    Implemented basic structure of game. Including Scenes, Level Manager, Sound Manager and UI.

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+	
+	public AudioClip[] levelMusicChangeArray;
+	
+	private AudioSource audioSource;
+	
+	void Awake () {
+		DontDestroyOnLoad (gameObject);
+		Debug.Log ("Do Not Destroy on Load: " + name);
+	}
+	
+	void Start () {
+		audioSource = GetComponent<AudioSource>();
+	}
+	
+	void OnLevelWasLoaded(int level){
+		AudioClip audioClip = levelMusicChangeArray[level];
+		Debug.Log("Playing clip: " + audioClip);
+		
+		if (audioClip) {
+			audioSource.Stop ();
+			audioSource.clip = audioClip;
+			audioSource.loop = true;
+			audioSource.Play();
+		}
+	}	
+}
