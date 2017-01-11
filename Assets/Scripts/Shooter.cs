46c3047384787f272a811e01fbd6a83bb0fea208
Ralph Sheehan <spectralrook@gmail.com>
2017-01-11 16:50:46 -0500

    Added Defender attack when Attackers are spawned for Defender lane.

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Shooter : MonoBehaviour {
 	
@@ -7,12 +8,63 @@ public class Shooter : MonoBehaviour {
 	
 	private GameObject projectileParent;
 	
+	private Animator animator;
+	
+	private Spawner mainSpawner;
+	
+	private GameObject myLaneSpawner;
+	
+	
 	void Start () {
+		animator = GameObject.FindObjectOfType<Animator>();
+		
+		mainSpawner = GameObject.FindObjectOfType<Spawner>();
+		
 		projectileParent = GameObject.Find("Projectiles");
 
 		if (!projectileParent) {
 			projectileParent = new GameObject("Projectiles");
 		}
+		
+		SetMyLaneSpawner();
+	}
+	
+	void Update () {
+
+		if (IsAttackerAheadInLane()) {
+			animator.SetBool ("isAttacking", true);
+		} else {
+			animator.SetBool ("isAttacking", false);
+		}
+	}
+	
+	// Look through all spawner's and set myLaneSpawner if found
+	void SetMyLaneSpawner () {
+		foreach (GameObject spawner in mainSpawner.spawners) {
+			if (spawner.transform.position.y == transform.position.y) {
+				Debug.Log(spawner.transform.position.y + "," + transform.position.y);					
+				myLaneSpawner = spawner;
+				return;			
+			}
+		}
+
+		Debug.LogError("Error: Unable to locate spawner in lane for " + this.name);
+	}
+	
+	bool IsAttackerAheadInLane() {
+		if (myLaneSpawner.transform.childCount <= 0) {
+			return false;
+		}
+		
+		foreach (Transform attacker in myLaneSpawner.transform) {
+			if (attacker.transform.position.x > transform.position.x) {
+				Debug.Log(attacker.name);
+				return true;
+			}
+		}
+		
+		// Attacker in lane, but behind us.
+		return false;
 	}
 	
 	private void FireGun () {
